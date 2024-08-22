47e8f798647aa4dbec134a58c3b8855b84123010
Antonio Vargas <avargasalmodovar@gmail.com>
2024-08-22 23:23:32 +0200

    Update Certifications.jsx

--- src/components/Certifications.jsx
+++ src/components/Certifications.jsx
@@ -2,6 +2,7 @@ import '../styles/Certifications.css'
 import EachCertification from './EachCertification.jsx';
 import p4b from '../documents/certificado_python_principiantes.pdf'
 import dA from '../documents/certificado_analisis_datos_python.pdf'
+import dAngular from '../documents/certificado_angular.pdf'
 import DsMl from '../documents/certificado_cienciaDatos_aprendizajeAutomatico_python.pdf'
 function Certifications(){
   return(
@@ -14,7 +15,7 @@ function Certifications(){
         <EachCertification reference={p4b} name='Python' description='Python for beginners' company='University of Malaga' />
         <EachCertification reference={DsMl} name='DS & ML' description='Data science and machine learning in Python' company='University of Malaga' />
         <EachCertification reference={dA} name='D. Analysis' description='Data Analysis in Python' company='University of Malaga' />
-        <EachCertification name='React' description='React for beginners' company='FreeCodeCamp' />
+        <EachCertification reference={dAngular} name='Angular' description='Level 2 angular training certification' company='Google developers' />
         <EachCertification name='Excel' description='Excel for everyone' company='University of Malaga' />
       </div>
     </div>
